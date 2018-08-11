c42efc20fdb5234b496b4274324069f885634a96
Eli Wemyss <eliwemyss@gmail.com>
2018-08-11 11:26:36 -0700

    fixed server.js spelling mistake

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express();
 app.use(morgan("common"));
 app.use(express.json());
 
-// you need to import `blogPostsRouter` router and route
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
