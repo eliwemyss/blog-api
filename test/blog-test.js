7002cbb6243e9ff0dabc5a8ec48cbc6c6813cd1e
Eli Wemyss <eliwemyss@gmail.com>
2018-08-15 21:13:20 -0700

    fixed failing tests

--- test/blog-test.js
+++ test/blog-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServier} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect;
 
@@ -12,7 +12,7 @@ describe('blog posts', function() {
 		return runServer();
 	});
 	after(function() {
-		return closeServier();
+		return closeServer();
 	});
 	it ('should list items on GET', function() {
 		return chai
@@ -22,7 +22,12 @@ describe('blog posts', function() {
 				expect(res).to.have.status(200);
 				expect(res).to.be.json;
 				expect(res.body.length).to.be.at.least(1);
-				const expectedKeys = ['id', 'title', 'content', 'author'];
+				const expectedKeys = [
+				 'id',
+				 'title',
+				 'content',
+				 'author',
+				 'publishDate'];
 				res.body.forEach(function(item) {
 					expect(item).to.be.a('object');
 					expect(item).to.include.keys(expectedKeys);
@@ -32,9 +37,11 @@ describe('blog posts', function() {
 
 	it('should add blog on POST', function() {
 		const newBlog = {
-			title: 'something', content: 'more stuff', author: 'somebody'
-		}
-		const expectedKeys = ['id', 'content'].concat(Object.keys(newBlog));
+			title: 'something', 
+			content: 'more stuff', 
+			author: 'somebody'
+		};
+		const expectedKeys = ['id', 'publishDate'].concat(Object.keys(newBlog));
 		return chai.request(app)
 			.post('/blog-posts')
 			.send(newBlog)
@@ -42,33 +49,45 @@ describe('blog posts', function() {
 				expect(res).to.have.status(201);
 				expect(res).to.be.json;
 				expect(res.body).to.be.a('object');
-				expect(res.body).to.include.keys('id', 'title', 'content', 'author');
-				expect(res.body.id).to.not.equal.equal(null);
-				expect(res.body).to.deep.equal(
-					Object.assign(newBlog, {id: res.body.id})
-					);
+				expect(res.body).to.include.keys(expectedKeys);
+				expect(res.body.title).to.equal(newBlog.title);
+				expect(res.body.content).to.equal(newBlog.content);
+				expect(res.body.author).to.equal(newBlog.author);
 			});
 	});
 
-	it('should update blog on PUT', function() {
-		const updateBlog = {
-			title: 'something',
-			content: 'more stuff',
-			author: 'somebody'
-		};
-		return chai.request(app)
-		.get('/blog-posts')
-		.then(function(res) {
-			updateBlog.id = res.body[0].id;
-
-			return chai.request(app)
-				.put(`/blog-posts/${updateBlog.id}`)
-				.send(updateBlog)
-				.then(function(res) {
-					expect(res).to.have.status(204);
+	it('should error if POST missing expected values', function() {
+		const badData = {};
+		return chai
+			.request(app)
+			.post('/blog-posts')
+			.send(badData)
+			.then(function(res) {
+				expect(res).to.have.status(400);
 			});
-		});
 	});
+
+	it("should update blog posts on PUT", function() {
+    	return (
+      		chai
+       		.request(app)
+        	.get("/blog-posts")
+        	.then(function(res) {
+         		 const updatedPost = Object.assign(res.body[0], {
+           			 title: "connect the dots",
+           			 content: "la la la la la"
+          		});
+        return chai
+            .request(app)
+            .put(`/blog-posts/${res.body[0].id}`)
+            .send(updatedPost)
+            .then(function(res) {
+              expect(res).to.have.status(204);
+            });
+        })
+    );
+  });
+
 
 	it("should delete posts on DELETE", function() {
     	return (
